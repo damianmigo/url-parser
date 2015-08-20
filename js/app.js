d53bccfb7d6b7b2aca0af6aa90ed5da08a434ab0
Damian Miranda <damian.miranda1@gmail.com>
2015-08-20 07:52:27 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+var app = angular.module('app', [])
+
+    .controller('UrlParserCtrl', ['$scope', function($scope) {
+
+        $scope.count = 0;
+
+        $scope.updateCount = function() {
+            var anchorElement = document.createElement('a');
+            anchorElement.href = $scope.url;
+
+            $scope.result = {
+                components: {
+                    hostname: anchorElement.hostname,
+                    port: anchorElement.port,
+                    protocol: anchorElement.protocol,
+                    pathname: anchorElement.pathname
+                },
+                params: []
+            };
+
+            var queryString = anchorElement.search;
+            if (queryString !== '') {
+                var paramList = queryString.substring(1).split('&');
+                angular.forEach(paramList, function(param) {
+                    var paramComponents = param.split('=');
+                    $scope.result.params.push({key: paramComponents[0], value: paramComponents[1]});
+                });
+            }
+        };
+    }])
+
+    .directive('urlParser', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/parser.html'
+        }
+    });
